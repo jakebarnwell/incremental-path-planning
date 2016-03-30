178b329b68682c6fead5fe745f548f4ec8440e67
16.412 Student <16.412 Student>
2016-03-30 00:37:51 -0400

    Cleaned up grid somewhat, starting to work again on World

--- code/grid.py
+++ code/grid.py
@@ -11,47 +11,37 @@ class WrongGridFormat(Exception):
         pass
     def __str__(self):
         return "Wrong grid format. Use 0 for free space, 1 for obstacle, S and G for start and goal, respectively, and R for robot."
-    
+
+class WrongCellType(Exception):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Invalid cell type. A cell type should be one of: " + repr(VALID_CELL_TYPES)
+
 PREFERRED_MAX_FIG_WIDTH = 12
 PREFERRED_MAX_FIG_HEIGHT = 8
-    
+
+# Easily refer to cell-types
+CELL_FREE = 0
+CELL_OBSTACLE = 1
+CELL_START = 7
+CELL_ROBOT = 8
+CELL_GOAL = 9
+VALID_CELL_TYPES = [CELL_FREE, CELL_OBSTACLE, CELL_START, CELL_ROBOT, CELL_GOAL]
+
 class Grid(object):
     def __init__(self, num_cols=10, num_rows=10, xy_limits=None, figsize=None):
         self.generate_grid(num_cols, num_rows)
 
+        # xy_limits are mostly for drawing purposes
         if not xy_limits:
             xy_limits = (0, num_cols), (0, num_rows)
-        self.set_xy_limits(*xy_limits)
+        self._set_xy_limits(*xy_limits)
+
         if not figsize:
-            figsize = self.calc_auto_figsize(xy_limits)
+            figsize = self._calc_auto_figsize(xy_limits)
         self.figsize = figsize
-        
-    @property
-    def size(self):
-        return self.grid_array.shape
-
-    def generate_grid(self, num_cols, num_rows):
-        self.grid_array = np.zeros([num_cols, num_rows])
-
-    def set_xy_limits(self, xlimits, ylimits):
-        num_cols, num_rows = self.size
-        if not isinstance(xlimits,tuple) or not len(xlimits)==2 \
-           or not isinstance(ylimits,tuple) or not len(ylimits)==2 \
-           or not xlimits[0] < xlimits[1] or not ylimits[0] < ylimits[1]:
-            raise ValueError('Specified xlimits or ylimits are not valid.')
-        self.xlimits, self.ylimits = xlimits, ylimits
-        minx, maxx = self.xlimits
-        miny, maxy = self.ylimits
-        self.cell_dimensions = (maxx-minx) / num_cols, (maxy-miny) / num_rows
-
-    def calc_auto_figsize(self, xy_limits):
-        (minx, maxx), (miny, maxy) = xy_limits
-        width, height = maxx - minx, maxy - miny
-        if width > height:
-            figsize = (PREFERRED_MAX_FIG_WIDTH, height * PREFERRED_MAX_FIG_WIDTH / width)
-        else:
-            figsize = (width * PREFERRED_MAX_FIG_HEIGHT / height, PREFERRED_MAX_FIG_HEIGHT)
-        return figsize
 
     @classmethod
     def create_from_file(grid_class, grid_file, xy_limits=None, figsize=None):
@@ -59,13 +49,14 @@ class Grid(object):
         grid = grid_class.create_from_str(gfile.read(), xy_limits=xy_limits, figsize=figsize)
         gfile.close()
         return grid
+
     @classmethod
     def create_from_str(grid_class, grid_str, xy_limits=None, figsize=None):
         # Don't ask.
         def string2value(s):
             string2valueDict = {"0":0, "1":1, "S":7, "G":9, "R":8}
             return string2valueDict[s]
-        
+
         lines = map(str.split, filter(lambda s:not s.startswith('#') and len(s)>0, map(str.strip,grid_str.split('\n'))))
         num_rows = len(lines)
         num_cols = len(lines[0])
@@ -86,9 +77,44 @@ class Grid(object):
 
         return grid
 
+    @property
+    def size(self):
+        return self.grid_array.shape
+
+    def get_cell(self, x, y):
+        return self.grid_array[x, y]
+
+    def set_cell(self, x, y, val):
+        self.grid_array[x, y] = val
+        return True
+
+    def generate_grid(self, num_cols, num_rows):
+        self.grid_array = np.zeros([num_cols, num_rows])
+
+    def _set_xy_limits(self, xlimits, ylimits):
+        num_cols, num_rows = self.size
+        if not isinstance(xlimits,tuple) or not len(xlimits)==2 \
+           or not isinstance(ylimits,tuple) or not len(ylimits)==2 \
+           or not xlimits[0] < xlimits[1] or not ylimits[0] < ylimits[1]:
+            raise ValueError('Specified xlimits or ylimits are not valid.')
+        self.xlimits, self.ylimits = xlimits, ylimits
+        minx, maxx = self.xlimits
+        miny, maxy = self.ylimits
+        self.cell_dimensions = (maxx-minx) / num_cols, (maxy-miny) / num_rows
+
+    def _calc_auto_figsize(self, xy_limits):
+        (minx, maxx), (miny, maxy) = xy_limits
+        width, height = maxx - minx, maxy - miny
+        if width > height:
+            figsize = (PREFERRED_MAX_FIG_WIDTH, height * PREFERRED_MAX_FIG_WIDTH / width)
+        else:
+            figsize = (width * PREFERRED_MAX_FIG_HEIGHT / height, PREFERRED_MAX_FIG_HEIGHT)
+        return figsize
+
+
     def add_random_obstacles(self, num_obs):
         """Clear grid and add random obstacles"""
-        
+
         # Only mess with cells that are completely free (i.e. a non-blocked cell
         #  that is not a robot, start, or goal cell)
         free_idx = zip(*np.where(self.grid_array == 0))
@@ -99,35 +125,20 @@ class Grid(object):
             self.grid_array[free_idx.pop(obs_idx)] = 1
             num_free = num_free - 1
 
-    def mark_obstacle_cell(self, x, y):
-        self.grid_array[x, y] = 1
+    def mark_cell_as(self, x, y, what_type):
+        if what_type not in VALID_CELL_TYPES:
+            raise WrongCellType
 
-    def mark_free_cell(self, x, y):
-        self.grid_array[x, y] = 0
-        
-    def mark_start_cell(self, x, y):
-        self.grid_array[x, y] = 7
-        
-    def mark_goal_cell(self, x, y):
-        self.grid_array[x, y] = 9
-        
-    def mark_robot_cell(self, x, y):
-        self.grid_array[x, y] = 8
+        self.grid_array[x, y] = what_type
+
+    def get_cells_of_type(self, what_type):
+        if what_type not in VALID_CELL_TYPES:
+            raise WrongCellType
+
+        return zip(*np.where(self.grid_array == what_type))
 
     def clear(self):
         self.grid_array = np.zeros([self.num_cols, self.num_rows])
-
-    def get_obstacles(self):
-        return zip(*np.where(self.grid_array == 1))
-    
-    def get_start(self):
-        return zip(*np.where(self.grid_array == 7))
-    
-    def get_goal(self):
-        return zip(*np.where(self.grid_array == 9))
-    
-    def get_robot(self):
-        return zip(*np.where(self.grid_array == 8))
 
     def cell_xy(self, ix, iy):
         """Returns the center xy point of the cell."""
@@ -136,7 +147,7 @@ class Grid(object):
         width, height = self.cell_dimensions
         return minx + (ix+0.5) * width, miny + (iy+0.5) * height
 
-    def cell_verts(self, ix, iy):
+    def _cell_verts(self, ix, iy):
         width, height = self.cell_dimensions
         x, y = self.cell_xy(ix, iy)
         verts = [(x + ofx*0.5*width, y + ofy*0.5*height) for ofx, ofy in [(-1,-1),(-1,1),(1,1),(1,-1)]]
@@ -179,23 +190,23 @@ class Grid(object):
         return plt.gca()
 
     def draw_obstacles(self, axes):
-        verts = [self.cell_verts(ix, iy) for ix,iy in self.get_obstacles()]
+        verts = [self._cell_verts(ix, iy) for ix,iy in self.get_cells_of_type(CELL_OBSTACLE)]
         collection_recs = PolyCollection(verts, facecolors='r')
         axes.add_collection(collection_recs)
-        
+
     def draw_start_goal(self, axes):
-        start_verts = [self.cell_verts(ix, iy) for ix,iy in self.get_start()]
-        goal_verts = [self.cell_verts(ix, iy) for ix,iy in self.get_goal()]
+        start_verts = [self._cell_verts(ix, iy) for ix,iy in self.get_cells_of_type(CELL_START)]
+        goal_verts = [self._cell_verts(ix, iy) for ix,iy in self.get_cells_of_type(CELL_GOAL)]
         collection_recs = PolyCollection(start_verts, facecolors='b')
         axes.add_collection(collection_recs)
         collection_recs = PolyCollection(goal_verts, facecolors='g')
         axes.add_collection(collection_recs)
-        
+
     def draw_robot(self, axes):
-        verts = [self.cell_verts(ix, iy) for ix,iy in self.get_robot()]
+        verts = [self._cell_verts(ix, iy) for ix,iy in self.get_cells_of_type(CELL_ROBOT)]
         collection_recs = PolyCollection(verts, facecolors='pink')
         axes.add_collection(collection_recs)
-        
+
     def draw_cell_circle(self, axes, xy, size=0.5, **kwargs):
         ix, iy = xy
         x, y = self.cell_xy(ix, iy)
@@ -210,19 +221,19 @@ class Grid(object):
     def to_graph(self):
         xmin, xmax = self.xlimits
         ymin, ymax = self.ylimits
-        
+
         def neighbors(xy):
             x,y = xy
             deltaX = [i for i in range(-1,2,1) if x+i >= xmin and x+i < xmax]
             deltaY = [j for j in range(-1,2,1) if y+j >= ymin and y+j < ymax]
-            
+
             n = []
             for dx in deltaX:
                 for dy in deltaY:
                     if not(dx == 0 and dy == 0): # gets rid of the original (x,y) case
                         n.append((x + dx, y + dy))
             return n
-            
+
         graph = Graph()
         node_positions = {}
         # Add all tiles as nodes
@@ -233,9 +244,9 @@ class Grid(object):
                 node_positions[(row,col)] = (row,col)
 
         graph.set_node_positions(node_positions)
-        
+
         # Add bi-directional edges between the 8 cardinal+ordinal neighbors
-        obstacles = self.get_obstacles()
+        obstacles = self.get_cells_of_type(CELL_OBSTACLE)
         for col in range(xmin, xmax):
             for row in range(ymin, ymax):
                 # For obstacle blocks, don't add any edges at all (I was considering
@@ -244,16 +255,5 @@ class Grid(object):
                 if (col, row) not in obstacles:
                     for neighbor in filter(lambda n: n not in obstacles, neighbors((col,row))):
                         graph.add_edge((col,row), neighbor, bidirectional=False)
-        
+
         return graph
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
--- code/search_problem.py
+++ code/search_problem.py
@@ -5,53 +5,54 @@ class IncrementalSearchProblem(object):
         self._start_node = start_node
         self._goal_node = goal_node
         self._world = world
-        
+
     def __repr__(self):
         return "{}(start_node={}, goal_node={}, world={})".format(type(self).__name__, repr(self._start_node), repr(self._goal_node), repr(self._world))
-    
+
     def __str__(self):
         repr_world = self._world if len(self._world) == 0 else "{...}"
         return "{}(start_node={}, goal_node={}, world={})".format(type(self).__name__, repr(self._start_node), repr(self._goal_node), repr_world)
-    
+
     def get_graph(self, robot_location):
         return self._world[robot_location]
-    
+
     def draw_state(self, grid, start=None, goal=None, robot_location=None, path=None):
         axes = grid.draw()
         if robot_location:
             grid.draw_cell_circle(axes, robot_location, color='red')
-                
+
     @property
     def start_node(self):
         return self._start_node
-    
+
     @property
     def goal_node(self):
         return self._goal_node
-    
+
     @start_node.setter
     def start_node(self, new_start_node):
         self._start_node = new_start_node
-        
+
     @goal_node.setter
     def goal_node(self, new_goal_node):
         self._goal_node = new_goal_node
 
 
+EPSILON_FRACTION = 0.01
 # Work in progress... need to do the right stuff with grids and make them
 #  mutable I think
 class World:
     """
-    A World object represents the belief-state of a robot. In 
+    A World object represents the belief-state of a robot. In
     particular, a World object stores what the world (as a grid)
     "looks like" to the robot at any particular point in time, as
     well as storing what the world "really" looks like.
-    
+
     A World object also stores the current position of the robot as
     a tuple (x,y), as well as the current "time" (time starts at 0 and
     is incremented by 1 whenever update_world is called), and the
     path travelled by the robot thus far in this World.
-    
+
     The only way to mutate anything in this class is via the update_world
     method.
     """
@@ -61,15 +62,15 @@ class World:
         # Stores what the robot "sees" or "believes" to be the world currently
         self._grid_belief_state = None
         self._update_belief_state()
-        
+
         # The current time step, robot's position, and path travelled so far
         self._time = 0
         self._robot_position = robot_start_position
         self._path_travelled = []
-        
+
         # The radius the robot can "see" for its belief state
         self._VISION_RADIUS = vision_radius
-        
+
     def update_world(self, next_robot_position):
         """
         Updates the ground truth grid AND the belief state grid,
@@ -77,16 +78,16 @@ class World:
         """
         _time += 1
         _path_travelled.append(next_robot_position)
-        
+
         # Update ground truth
         pass
-    
+
         # Re-calculate belief state
         self._update_belief_state()
-        
+
         # Graph-ify the belief state and return it
         return self._grid_belief_state.to_graph()
-        
+
     def _update_belief_state(self):
         """
         Updates the robot's belief state of the world. This
@@ -94,66 +95,27 @@ class World:
         e.g. what the robot can "see" from where it's at.
         """
         grid_array = self._grid_ground_truth.grid_array
-        size = self._grid_ground_truth.size
-        print size
+        num_cols, num_rows = self._grid_ground_truth.size
         x, y = self._robot_position
         cx, cy = self._grid_ground_truth.cell_xy(x, y)
-        xmax, ymax = size
-        for i in xrange(xmax):
-            for j in xrange(ymax):
+        for col in xrange(num_cols):
+            for row in xrange(num_rows):
                 ix, iy = self._grid_ground_truth.cell_xy(i, j)
-                if (cx - ix)**2 + (cy - jy)**2 <= self._VISION_RADIUS**2:
+                if (cx - ix)**2 + (cy - jy)**2 <= (self._VISION_RADIUS**2) * (1 + EPSILON_FRACTION):
                     self._grid_belief_state[i, j] = grid_array
                 else:
                     self._grid_belief_state[i, j] = 0 # It thinks it's clear
         return True
-            
-        
+
+
     @property
     def robot_position(self):
         return _robot_position
-    
+
     @property
     def time(self):
         return _time
-    
+
     @property
     def path_travelled(self):
         return _path_travelled
-
-# class World:
-#     __init__(grid)
-#     # --> graph
-#     robot_xy ;; current tuple (x,y) of robot position
-#     path_travelled ;; path the robot has travelled [(x,y),...] updated in backgroudn
-
-# d*lite has to call update_world(robot_position_node);; returns the new graph
-#  ground_truth_graph
-#  visible_world_graph
-
-# (3,4)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
